4429f7e6930d53f4a3d68cc7b155b961b48fda04
Jeffrey Lin <jlin2713@usc.edu>
2023-01-26 15:15:11 -0800

    Create board

---
+++ CSCI-526/Assets/Scripts/GridManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tile;
+
+    [SerializeField] private Transform _camera;
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var tile = Instantiate(_tile, new Vector3(x, y), Quaternion.identity);
+                _tile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                _tile.Init(isOffset);
+            }
+        }
+
+        _camera.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CSCI-526/Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _base, _offset;
+
+    [SerializeField] private SpriteRenderer _renderer;
+
+    public void Init(bool isOffset)
+    {
+        _renderer.color = isOffset ? _offset : _base;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
