18727ffb0913bd6928150aa696c13557e8222aa4
Nicholas Owen Weber <noweber@gmail.com>
2023-01-29 01:25:23 -0800

    Added Reload Scene Script for Testing

---
+++ CSCI-526/Assets/TerrainPrototype/Scripts/ReloadScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
