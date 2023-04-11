eb57d07f2836b64dd73b73b884bcdea479befa28
Nicholas Owen Weber <noweber@gmail.com>
2023-04-11 08:15:37 -0700

    Update PromptBoxClicked.cs

--- CSCI-526/Assets/Scripts/Levels/PromptBoxClicked.cs
+++ CSCI-526/Assets/Scripts/Levels/PromptBoxClicked.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PromptBoxClicked : MonoBehaviour
@@ -10,16 +8,5 @@ public class PromptBoxClicked : MonoBehaviour
     {
         Debug.Log("CLICKED");
         promptObject.SetActive(false);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
