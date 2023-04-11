cc6a36d9efab69317a3cf6f68b4a56cfcebc661e
Nicholas Owen Weber <noweber@gmail.com>
2023-04-11 08:10:01 -0700

    Triangle Update

--- CSCI-526/Assets/Scripts/Units/Triangle.cs
+++ CSCI-526/Assets/Scripts/Units/Triangle.cs
@@ -1,7 +1,5 @@
-﻿using Assets.Scripts.Piece;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Assets.Scripts.Units
 {
@@ -14,33 +12,7 @@ namespace Assets.Scripts.Units
 
         public override List<Tuple<int, int>> LegalMoves(int boardWidth, int boardHeight)
         {
-            List<Tuple<int, int>> legalSpots = new List<Tuple<int, int>>();
-            var pos = this.transform.position;
-            if (this.TriangleAbilityCheck())
-            {
-                // MenuManager.Instance.ShowAbilityButton();
-            }
-            return legalSpots;
-        }
-
-        private bool TriangleAbilityCheck()
-        {
-            var pos = this.transform.position;
-            var adjList = this.AdjacentAllies();
-            var lvlMono = LevelMono.Instance;
-            if (adjList != null)
-            {
-                foreach (Tuple<int, int> coord in adjList)
-                {
-                    if (lvlMono.GetPiece(coord) != null && lvlMono.GetPiece(coord).IsCircle())// && string.Equals(lvlModel.TryGetUnit(coord).Name(), UnitType.Circle.ToString()))
-                    {
-                        Debug.Log("Set Triangle Ability True");
-                        //return true;
-                        return true;
-                    }
-                }
-            }
-            return false;
+            return new List<Tuple<int, int>>();
         }
 
         public override List<Tuple<int, int>> GetVisibleArea(int range)
@@ -66,6 +38,5 @@ namespace Assets.Scripts.Units
 
             return visibleArea;
         }
-
     }
 }
