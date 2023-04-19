89a155dffea5d6db935f3b438d3a24793e93b03a
Josh Maranan <jmaranan.1999@gmail.com>
2023-04-19 15:00:06 -0700

    End Turn button blinks, started playing with color when player unit can't move

--- CSCI-526/Assets/Scripts/Units/HighlightUnit.cs
+++ CSCI-526/Assets/Scripts/Units/HighlightUnit.cs
@@ -1,15 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-// Attached to CanMove and CantMove game objects in each unit prefab
+// Attached to CanMove game object in each unit prefab and EndTurn button in MenuManager
 public class HighlightUnit : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer highlight;
+    [SerializeField] private SpriteRenderer highlight;      // For CanMove
+    [SerializeField] private Image btn;     // For End Turn
 
+    [SerializeField] private float c = 1;       // For End Turn r, g, b
+    private bool increasingC = false;
+    public bool moused = false;
+    private IEnumerator FadeEndTurn()
+    {
+        while (true)
+        {
+            if (c >= 0.9f)
+            {
+                increasingC = false;
+            }
+            else if (c <= 0.7f)
+            {
+                increasingC = true;
+            }
+            if (increasingC)
+            {
+                c += 0.07f;
+            }
+            else
+            {
+                c -= 0.07f;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+
+    private void OnMouseEnter()
+    {
+        moused = true;
+    }
+    private void OnMouseExit()
+    {
+        moused = false;
+    }
+    private void Awake()
+    {
+        if(btn != null)
+        {
+            StartCoroutine(FadeEndTurn());
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        highlight.color = new Color(highlight.color.r, highlight.color.g, highlight.color.b, GameManagerChain.Instance.movableAlpha);
+        if(highlight != null)
+        {
+            highlight.color = new Color(highlight.color.r, highlight.color.g, highlight.color.b, GameManagerChain.Instance.movableAlpha);
+        }
+        if(btn != null && !moused)
+        {
+            btn.color = new Color(c, c, c, 1);
+        }
     }
 }
